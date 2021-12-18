3df5d590c0a80eab9709e976231aa9d9e7f1b5b9
hayryne <mikko.hayrynen@hotmail.com>
2021-12-18 16:23:49 +0200

    add vue to render data

--- public/index.js
+++ public/index.js
@@ -1,8 +1,19 @@
-const getData = async () => {
-    const res = await fetch('/data', {})
-    const data = await res.json()
-
-    console.log(data)
-}
-
-getData()
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            data: []
+        }
+    },
+    created() {
+        this.getData()
+    },
+    methods: {
+        async getData() {
+            const res = await fetch('/data', {})
+            const data = await res.json()
+        
+            this.data = data
+        }
+    }
+})
