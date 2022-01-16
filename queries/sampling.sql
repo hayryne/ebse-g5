bffd45b3b28726a45be5dc5e83942b20ee0e7bb8
armand-colin <56818352+armand-colin@users.noreply.github.com>
2022-01-16 13:57:25 +0100

    edited sampling query to take satd_count into account

--- queries/sampling.sql
+++ queries/sampling.sql
@@ -2,22 +2,25 @@
 Partitions over linkedness in order to obtain amounts proportional to chosen
 sampling strata; e.g. 100 samples with linkedness = 1
 */
-select t.*
-from (SELECT l.*,
-      row_number() over (partition by linked order by random()) as seqnum
-      FROM (
-        SELECT *,
-        CASE 
-          WHEN jira_number IS NOT NULL AND issue_number IS NULL THEN 1
-          WHEN issue_number IS NOT NULL AND jira_number IS NULL THEN 2
-          WHEN issue_number IS NOT NULL AND jira_number IS NOT NULL THEN 3
-          ELSE 0
-        END AS linked
-        FROM github_jira_link
-      ) l
-     ) t
-/* Proportional to a sample of 5000 */
-where linked = 0 and seqnum <= 2950 or
-      linked = 1 and seqnum <= 1950 or
-      linked = 2 and seqnum <= 100 or
-      linked = 3 and seqnum <= 1;
+SELECT * FROM (
+  SELECT 
+    *, 
+    row_number() 
+      OVER (
+        PARTITION BY linked 
+        ORDER BY random()
+      ) as seqnum 
+
+  FROM github_jira_link
+)
+
+WHERE satd_count > 2 AND
+      /* Proportional to a sample of 5000 */
+      (
+        linked = 0 AND seqnum <= 2950 OR
+        linked = 1 AND seqnum <= 1950 OR
+        linked = 2 AND seqnum <= 100 OR
+        linked = 3 AND seqnum <= 1
+      )
+
+LIMIT 10
