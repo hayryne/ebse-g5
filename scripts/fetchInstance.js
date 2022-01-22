544c8ee2c96655c1624bffce0751cd68acaf4664
armand-colin <56818352+armand-colin@users.noreply.github.com>
2022-01-22 12:48:08 +0100

    Update fetchInstance.js

--- scripts/fetchInstance.js
+++ scripts/fetchInstance.js
@@ -1,7 +1,28 @@
 const { fetch } = require('../db')
 
-async function fetchInstance(id) {  
+async function fetchInstance(id) {
     const entry = (await fetch(`SELECT * from github_jira_link WHERE id = ${id}`))[0]
+
+    const labels = {
+        1: "code-design",
+        2: "documentation",
+        3: "test",
+        4: "requirement"
+    }
+
+    const mapper = (data) => {
+        if (!data.label_id) {
+            delete data.label_id
+            const keys = Object.keys(data)
+            if (keys.length == 1)
+                return data[keys[0]]
+            return data
+        }
+
+        const label = labels[data.label_id]
+        delete data.label_id
+        return { ...data, label: label }
+    }
 
     const [
 
@@ -20,21 +41,91 @@ async function fetchInstance(id) {
 
     ] = await Promise.all([
 
-        fetch(`SELECT comment from git_comment WHERE sha = "${entry.sha}" AND repo_id = ${entry.repo_id}`, d => d.comment),
-        fetch(`SELECT summary, message from git_commit WHERE sha = "${entry.sha}" AND repo_id = ${entry.repo_id}`),
+        fetch(`
+            SELECT git_comment.comment, git_comment_satd.label_id 
+            FROM git_comment 
+            LEFT JOIN git_comment_satd 
+            ON git_comment.id = git_comment_satd.id 
+            WHERE git_comment.sha = "${entry.sha}" AND git_comment.repo_id = ${entry.repo_id}
+        `, mapper),
+
+        fetch(`
+            SELECT git_commit.summary, git_commit.message, git_commit_satd.label_id 
+            FROM git_commit 
+            LEFT JOIN git_commit_satd 
+            ON git_commit.sha = git_commit_satd.sha 
+            WHERE git_commit.sha = "${entry.sha}" AND git_commit.repo_id = ${entry.repo_id}
+        `, mapper),
 
         // // Prevent from doing any computation if no issue
         entry.issue_number === null ? null :
-                fetch(`SELECT * FROM github_issue WHERE number = ${entry.issue_number} AND repo_id = ${entry.repo_id}`),
+            fetch(`
+                gibberish SELECT github_issue.title, github_issue.body, github_issue_satd.label_id 
+                FROM github_issue
+                LEFT JOIN github_issue_satd
+                ON github_issue_satd.number = github_issue.number
+                AND github_issue_satd.repo_id = github_issue.repo_id
+                WHERE github_issue.number = ${entry.issue_number} AND github_issue.repo_id = ${entry.repo_id}
+            `, mapper),
+
         entry.issue_number === null ? null :
-            fetch(`SELECT * FROM github_issue_comment WHERE number = ${entry.issue_number} AND repo_id = ${entry.repo_id}`),
+            fetch(`
+                SELECT github_issue_comment.body, github_issue_satd.label_id 
+                FROM github_issue_comment
+                LEFT JOIN github_issue_satd
+                ON github_issue_satd.number = github_issue_comment.number AND
+                github_issue_satd.repo_id = github_issue_comment.repo_id AND
+                github_issue_satd.id = github_issue_comment.id
+                WHERE github_issue_comment.number = ${entry.issue_number} AND github_issue_comment.repo_id = ${entry.repo_id}
+            `, mapper),
 
-        fetch(`SELECT title, body FROM github_pull WHERE number = ${entry.pull_number} AND repo_id = ${entry.repo_id}`),
-        fetch(`SELECT body FROM github_pull_comment WHERE number = ${entry.pull_number} AND repo_id = ${entry.repo_id}`, d => d.body),
-        fetch(`SELECT body FROM github_pull_review WHERE number = ${entry.pull_number} AND repo_id = ${entry.repo_id}`, d => d.body),
+        fetch(`
+            SELECT github_pull.title, github_pull.body, github_pull_satd.label_id
+            FROM github_pull
+            LEFT JOIN github_pull_satd
+            ON github_pull.number =  github_pull_satd.number AND
+            github_pull.repo_id = github_pull_satd.repo_id
+            WHERE github_pull.number = ${entry.pull_number} AND github_pull.repo_id = ${entry.repo_id}
+        `, mapper),
 
-        fetch(`SELECT summary, description FROM jira_issue WHERE number = ${entry.jira_number} AND identifier = "${entry.jira_identifier}"`),
-        fetch(`SELECT body FROM jira_issue_comment WHERE number = ${entry.jira_number} AND identifier = "${entry.jira_identifier}"`, d => d.body)
+        fetch(`
+            SELECT github_pull_comment.body, github_pull_satd.label_id
+            FROM github_pull_comment
+            LEFT JOIN github_pull_satd
+            ON github_pull_comment.id = github_pull_satd.id
+            AND github_pull_comment.number = github_pull_satd.number
+            AND github_pull_comment.repo_id = github_pull_satd.repo_id
+            WHERE github_pull_comment.number = ${entry.pull_number} AND github_pull_comment.repo_id = ${entry.repo_id}
+        `, mapper),
+
+        fetch(`
+            SELECT github_pull_review.body, github_pull_satd.label_id 
+            FROM github_pull_review
+            LEFT JOIN github_pull_satd
+            ON github_pull_review.id = github_pull_satd.id
+            AND github_pull_review.number = github_pull_satd.number
+            AND github_pull_review.repo_id = github_pull_satd.repo_id
+            WHERE github_pull_review.number = ${entry.pull_number} AND github_pull_review.repo_id = ${entry.repo_id}
+        `, mapper),
+
+        fetch(`
+            SELECT jira_issue.summary, jira_issue.description, jira_issue_satd.label_id 
+            FROM jira_issue
+            LEFT JOIN jira_issue_satd
+            ON jira_issue.number = jira_issue_satd.number
+            AND jira_issue.identifier = jira_issue_satd.identifier
+            WHERE jira_issue.number = ${entry.jira_number} AND jira_issue.identifier = "${entry.jira_identifier}"
+        `, mapper),
+
+        fetch(`
+            SELECT jira_issue_comment.body, jira_issue_satd.label_id
+            FROM jira_issue_comment
+            LEFT JOIN jira_issue_satd
+            ON jira_issue_comment.id = jira_issue_satd.id
+            AND jira_issue_comment.identifier = jira_issue_satd.identifier
+            AND jira_issue_comment.number = jira_issue_satd.number
+            WHERE jira_issue_comment.number = ${entry.jira_number} AND jira_issue_comment.identifier = "${entry.jira_identifier}"
+        `, mapper)
 
     ])
 
